c5e2946f75005139e4ec13f5f4b215d2bbd9577b
soufianebt <soufiane.boutahiri@um5.net.ma>
2023-05-27 02:41:30 +0100

    design dashboard

--- src/main/java/com/FSTM/syndica/Model/Meeting.java
+++ src/main/java/com/FSTM/syndica/Model/Meeting.java
@@ -3,6 +3,8 @@ package com.FSTM.syndica.Model;
 import java.util.Date;
 
 public class Meeting {
+    public int meetingId;
+    public String immubleId;
     public Date meetingDate;
     public String syndicId;
     public String title;
